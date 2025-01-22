1abc0da80c335c4d7497025befd65c85350e2860
jajerrr <jaajer@gmail.com>
2025-01-22 18:08:06 +0700

    pano

---
+++ components/pano360/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="viewer" style="width: 100vw; height: 100vh;"></div>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue';
+  import { Viewer } from 'photo-sphere-viewer';
+  import 'photo-sphere-viewer/dist/photo-sphere-viewer.css';
+  
+  onMounted(() => {
+    new Viewer({
+      container: document.getElementById('viewer'),
+      panorama: '/image/Muslim.png', // Replace with your panorama image
+    });
+
+    
+  });
+  </script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <PanoramaViewer />
+  </div>
+</template>
+
+<script setup>
+import PanoramaViewer from '~/components/pano360/index.vue';
+</script>
